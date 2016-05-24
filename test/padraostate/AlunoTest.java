179c9da8e55b91ef602b075c6286b567142e3b18
user <user@localhost>
2016-05-24 17:21:21 -0300

    Inicio1

--- test/padraostate/AlunoTest.java
+++ test/padraostate/AlunoTest.java
@@ -28,7 +28,7 @@ public class AlunoTest extends TestCase {
     
     public void testEstadoAluno_EstadoMatriculadoParaMatriculado() {
         aluno.setEstado(new EstadoMatriculado());
-        assertEquals("Matricula não efetuada uma vez que o aluno já está matriculado.", aluno.matricular());
+        assertEquals("Matricula não efetuada uma vez que o aluno já está matriculado. ", aluno.matricular());
     }
     
     
